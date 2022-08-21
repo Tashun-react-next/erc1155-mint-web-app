33bab940a7f29fcfd365f987e4964401cb74bdb1
tashun <tashun.chamara@gmail.com>
2022-08-21 15:54:59 +0530

    popup and card separated nav bar hide added

--- components/side-navbar/item-navbar.js
+++ components/side-navbar/item-navbar.js
@@ -6,7 +6,7 @@ export const ItemNavbar = (props) => {
         <li className="nav-item">
             <a href="#" className="nav-link active" aria-current="page">
                 <svg className="bi pe-none me-2" width="16" height="16">
-                    <use xlinkHref="#home"></use>
+                    <use xlinkHref="#home1"></use>
                 </svg>
                 {props.name}
             </a>
--- components/side-navbar/side-navbar.js
+++ components/side-navbar/side-navbar.js
@@ -1,73 +1,72 @@
 import Link from "next/link";
 import Image from "next/image";
+import {useEffect, useState} from "react";
+import navigationData from "../../data/navigationData.json";
 
 export const SideNavbar = (props) => {
+    const [activeIndex, setActiveIndex] = useState(0);
+    const [navigationData, setNavigationData] = useState([]);
 
-    const getNavBarItem = (isActive, name) => {
+    useEffect(() => {
+        setNavigationData(props?.navigationData? props.navigationData : [])
+    }, [props.navigationData]);
+
+    const getNavBarItem = (name, idx) => {
+        // setActiveIndex(idx);
         return (
-            <li className="nav-item">
-                <a href="#" className="nav-link" aria-current="page">
+            <li key={idx} className="nav-item" onClick={() => activeSelectedNavItem(idx)}>
+                <a href="#" className={props.showSideNavBar ? "nav-link active" : "nav-link text-white"} aria-current="page">
                     <svg className="bi pe-none me-2" width="16" height="16">
-                        <use xlinkHref="#home"></use>
+                        <use xlinkHref="#home2"></use>
                     </svg>
-                    {name}
+                    { (props.showSideNavBar)? {name} : "...."}
+                </a>
+            </li>
+        )
+    }
+
+    const activeSelectedNavItem = (idx)=> {
+        if (props.showSideNavBar) {
+            navigationData[idx].isSelected =true;
+            navigationData[activeIndex].isSelected =false;
+            setActiveIndex(idx);
+            props.setRoute('mint/' + idx);
+            console.log(idx);
+        } else {
+            props?.setShowSideNavBar(!props?.showSideNavBar)
+        }
+    }
+
+    const getInActiveNavBarItem = (value, idx) => {
+        return (
+            <li key={idx} className="nav-item"  onClick={() => activeSelectedNavItem(idx)} >
+                <a  className={value.isSelected  ? "nav-link active" : "nav-link text-white"}>
+
+                        {props.showSideNavBar? value.displayName : "...."}
+
                 </a>
             </li>
         )
     }
 
     return (
-        <div className="d-flex flex-column flex-shrink-0 p-3 text-bg-dark" style={{"width":"280px", height: "100vh"}}>
+        <div className="d-flex flex-column flex-shrink-0 p-3 text-bg-dark" style={{height: "100vh"}}>
             <div className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-                    <Image src="/icons/1.png"
-                    width="50px"
-                    height="50px"
-                    alt="Expand"
-                    />
-                <span className="ps-2 fs-4">Mint your Stuff</span>
+                <Image src="/icons/1.png" onClick={()=>props?.setShowSideNavBar(!props?.showSideNavBar)}
+                       width="50px"
+                       height="50px"
+                       alt="Expand"
+                />
+                {props?.showSideNavBar &&<span className="ps-2 fs-4">Mint your Stuff</span>}
             </div>
             <hr/>
             <ul className="nav nav-pills flex-column mb-auto">
-                <li className="nav-item">
-                    <a href="#" className="nav-link active" aria-current="page">
-                        <svg className="bi pe-none me-2" width="16" height="16">
-                            <use xlinkHref="#home"></use>
-                        </svg>
-                        Home
-                    </a>
-                </li>
-                <li>
-                    <a href="#" className="nav-link text-white">
-                        <svg className="bi pe-none me-2" width="16" height="16">
-                            <use xlinkHref="#speedometer2"></use>
-                        </svg>
-                        Dashboard
-                    </a>
-                </li>
-                <li>
-                    <a href="#" className="nav-link text-white">
-                        <svg className="bi pe-none me-2" width="16" height="16">
-                            <use xlinkHref="#table"></use>
-                        </svg>
-                        Orders
-                    </a>
-                </li>
-                <li>
-                    <a href="#" className="nav-link text-white">
-                        <svg className="bi pe-none me-2" width="16" height="16">
-                            <use xlinkHref="#grid"></use>
-                        </svg>
-                        Products
-                    </a>
-                </li>
-                <li>
-                    <a href="#" className="nav-link text-white">
-                        <svg className="bi pe-none me-2" width="16" height="16">
-                            <use xlinkHref="#people-circle"></use>
-                        </svg>
-                        Customers
-                    </a>
-                </li>
+                {
+                    navigationData?.map((value, index) => {
+                            return  getInActiveNavBarItem(value, index)
+                        }
+                    )
+                }
             </ul>
             <hr/>
             <div className="dropdown">
